52634bd78e8762f0bbe8ec21f9753256551eb841
Michael Burrowes <michael@burrowesfamily.com>
2015-11-24 14:32:47 -0500

    Initial/cleanup

--- GameJam2015/Assets/World.cs
+++ GameJam2015/Assets/World.cs
@@ -3,15 +3,12 @@ using System.Collections;
 
 public class World : MonoBehaviour 
 {
-
-	// Use this for initialization
-	void Start () 
+	void Start() 
    {
 	
 	}
 	
-	// Update is called once per frame
-	void Update () 
+	void Update() 
    {
 	
 	}

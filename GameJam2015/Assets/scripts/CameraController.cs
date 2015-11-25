d037e24f7fb1747db571211819edb4a1227d40e2
Buzz Burrowes <buzz@harmonixmusic.com>
2015-11-25 12:02:40 -0500

    Music and camera

---
+++ GameJam2015/Assets/scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour 
+{
+   public Transform LookAtObject;
+    
+   void Start() 
+   {
+	
+   }
+	
+   void Update()
+   {
+	   Vector3 newPos = transform.position;
+      newPos.x = LookAtObject.position.x;
+      newPos.y = LookAtObject.position.y;
+      transform.position = newPos;
+   }
+}

c31531819617ad5ced4f702f5c086f124b26ee1d
Buzz Burrowes <buzz@harmonixmusic.com>
2015-11-25 11:47:19 -0500

    Basic Controller

---
+++ GameJam2015/Assets/scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour 
+{
+   Rigidbody2D mRigidBody;
+   public float VelocityScale = 1.0f;
+   void Start()
+   {
+	   mRigidBody = GetComponent<Rigidbody2D>();   
+	}
+	
+	void Update() 
+   {
+      float x = Input.GetAxis("Horizontal");
+      float y = Input.GetAxis("Vertical");
+      mRigidBody.velocity = new Vector2(x * VelocityScale, y * VelocityScale);
+	}
+}

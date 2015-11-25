1343b4fc90b04144e83adecd6da34f30367e84ff
Buzz Burrowes <buzz@harmonixmusic.com>
2015-11-25 13:59:03 -0500

    Beginning of Data Model

---
+++ GameJam2015/Assets/scripts/DataModel/CollectableItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class CollectableItem
+{
+   private string mName;
+   public string Name
+   {
+      get { return mName; }
+      set { mName = value; }
+   }
+
+   public CollectableItem(string name)
+   {
+      mName = name;
+   }
+}
---
+++ GameJam2015/Assets/scripts/DataModel/World.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class World
+{
+   List<CollectableItem> mCollectibles = new List<CollectableItem>();
+   
+   public World()
+   {
+   }   
+}

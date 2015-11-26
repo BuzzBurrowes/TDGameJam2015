2a8c97f6f4977787665ba4fb9816486d033bed74
Buzz Burrowes <buzz_burrowes@yahoo.com>
2015-11-26 12:24:25 -0500

    Some UI work

---
+++ GameJam2015/Assets/scripts/MenuController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuController : MonoBehaviour 
+{
+   public Canvas InventoryMenu;
+
+   void Start() 
+   {
+	
+   }
+	
+   void Update()
+   {
+      Debug.Log(Input.GetAxis("InventoryMenu"));
+      if (Input.GetButtonDown("InventoryMenu"))
+      {
+         if (!InventoryMenu.gameObject.activeInHierarchy)
+            InventoryMenu.gameObject.SetActive(true);
+         else
+            InventoryMenu.gameObject.SetActive(false);
+      }
+   }
+}
